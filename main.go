142995176d7025aa87d995caeba518b4e7e7f35a
Kamaljeet-01 <kamaljeetkamaljeet548@gmail.com>
2025-06-17 23:37:13 +0530

    uploading the space-connect code

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	// Render the home html page from static folder
+	http.ServeFile(w, r, "static/index.html")
+}
+
+func indexPage(w http.ResponseWriter, r *http.Request) {
+	// Render the course html page
+	http.ServeFile(w, r, "static/index.html")
+}
+
+func aboutPage(w http.ResponseWriter, r *http.Request) {
+	// Render the about html page
+	http.ServeFile(w, r, "static/about.html")
+}
+
+func contactPage(w http.ResponseWriter, r *http.Request) {
+	// Render the contact html page
+	http.ServeFile(w, r, "static/contact.html")
+}
+
+func main() {
+
+	// Serve static assets (JS/CSS/Images) under /static/ path
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/index", indexPage)
+	http.HandleFunc("/about", aboutPage)
+	http.HandleFunc("/contact", contactPage)
+
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
